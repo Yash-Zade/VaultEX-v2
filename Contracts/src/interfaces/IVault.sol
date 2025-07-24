23e71612a9147b7d1459e767eb66c9f1ea5e3ebb
Yash Zade <yashmzade@gmail.com>
2025-07-24 22:13:51 +0530

    Create Interfaces for PriceFeed, Vault and PositionNFT

---
+++ Contracts/src/interfaces/IVault.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IVault {
+    // Struct for user balances
+    struct UserData {
+        uint256 depositedBalance;
+        uint256 lockedBalance;
+        uint256 availableBalance;
+    }
+
+    // External functions
+    function depositCollateral(uint256 _amount) external;
+    function withdrawCollateral(uint256 _amount) external;
+    function lockCollateral(address _user, uint256 _amount) external;
+    function unlockCollateral(address _user, uint256 _amount) external;
+    function transferCollateral(address _from, address _to, uint256 _amount) external;
+    function setPositionManager(address _positionManager) external;
+
+    // View functions
+    function getUserCollateral() external view returns (UserData memory);
+    function getTotalLiquidity() external view returns (uint256);
+    function getUtilizationRate() external view returns (uint256);
+}
