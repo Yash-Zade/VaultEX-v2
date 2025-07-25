52da086f3cfc6ae21641b7b63644b5341c204dda
Yash Zade <yashmzade@gmail.com>
2025-07-25 13:58:32 +0530

    Update in PositionNFT

--- Contracts/src/interfaces/IPositionNFT.sol
+++ Contracts/src/interfaces/IPositionNFT.sol
@@ -15,12 +15,12 @@ interface IPositionNFT {
     }
 
     // External functions
-    function mintPosition(address to, uint256 collateral, uint8 leverage, uint256 entryPrice, bool isLong) external returns (uint256);
+    function mintPosition(address to, uint256 collateral, uint8 leverage, uint256 entryPrice,int entryFundingRate, bool isLong) external returns (uint256);
     function burnPosition(uint256 tokenId) external;
     function updatePosition(uint256 tokenId, uint256 newSize, uint256 newCollateral) external;
 
     // View functions
-    function getPosition(uint256 tokenId) external view returns (uint256, uint256, uint8, uint256, uint256, bool, string memory);
+    function getPosition(uint256 tokenId) external view returns (uint256, uint256, uint8, uint256, uint256, int, bool, string memory);
     function getUserPositions(address user) external view returns (uint256[] memory);
     function ownerOf(uint256 tokenId) external view returns (address);
 }
