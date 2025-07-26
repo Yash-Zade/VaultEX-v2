ffd1500fe87eba66fd3dd7ee8cde08faddc8f3ca
Yash Zade <yashmzade@gmail.com>
2025-07-26 17:25:22 +0530

    fixed import error in IPriceFeed

--- Contracts/src/interfaces/IPriceFeed.sol
+++ Contracts/src/interfaces/IPriceFeed.sol
@@ -1,5 +1,5 @@
-// SPDX-License-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 interface IPriceFeed {
     function getLatestPrice() external view returns (int256);
