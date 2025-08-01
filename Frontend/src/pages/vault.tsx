9da4877c275522059fb79a2feedaa12047e42efe
Yash <yashmzade@gmail.com>
2025-08-01 14:03:39 +0530

    Create vault page for the appliation to manage the collateral

---
+++ Frontend/src/pages/vault.tsx
@@ -0,0 +1,13 @@
+import Vault from "@/components/Vault";
+import DefaultLayout from "@/layouts/default";
+
+
+export default function TradePage() {
+  return (
+    <DefaultLayout>
+      <section>
+          <Vault/>
+      </section>
+    </DefaultLayout>
+  );
+}
