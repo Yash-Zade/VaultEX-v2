9334f76ca967caf5d3254287e0f6fb19efb33087
Yash <yashmzade@gmail.com>
2025-08-07 19:01:09 +0530

    Update in interfaces

--- Contracts/src/interfaces/IPriceFeed.sol
+++ Contracts/src/interfaces/IPriceFeed.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.0;
 interface IPriceFeed {
     function getLatestPrice() external view returns (int256);
     function getDecimals() external view returns (uint8);
+    function setAggregator(address _aggregator) external;
 }
